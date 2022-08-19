1b98896279fcb9d545d7e2b17bfbaed10b2538bc
Abhineet <abhineetkr123@gmail.com>
2022-08-19 18:55:10 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+    <header>
+      <div className="nav-area">
+        <a href="/#" className="logo">
+          ClickCart
         </a>
-      </header>
+        <Navbar />
+      </div>
+    </header>
+    <div>
+      <img src="https://beebom.com/wp-content/uploads/2020/07/fake-flipkart-clones.jpg?quality=75&strip=all"alt=""></img>
     </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/menuItems.js
@@ -0,0 +1,46 @@
+export const menuItems = [
+    {
+      title: "Home",
+    },
+    {
+      title: "Fashion",
+      submenu: [
+        {
+          title: "Kids wear",
+        },
+        {
+          title: "Men's  Wear",
+          submenu: [
+            {
+              title: "mens tees",
+            },
+            {
+              title: "mens pants",
+              submenu: [
+                {
+                  title: "Formal",
+                },
+                {
+                  title: "Casual",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: "Women's Wear",
+        },
+      ],
+    },
+    {
+      title: "Electronics",
+      submenu: [
+        {
+          title: "Mobiles",
+        },
+        {
+          title: "Laptops",
+        },
+      ],
+    },
+  ];
